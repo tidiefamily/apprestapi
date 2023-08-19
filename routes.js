86e6841372fdbaa2c7db7db33390b649460746b4
TIDIE <yuldijasman@gmail.com>
2023-08-19 17:00:31 +0700

    berhasil menampilkan data berdasarkan id

--- routes.js
+++ routes.js
@@ -7,4 +7,5 @@ module.exports = function (app) {
   app.route("/").get(jsonku.index);
 
   app.route("/tampil").get(jsonku.tampilsemuamahasiswa);
+  app.route("/tampil/:id").get(jsonku.tampilberdasarkanid);
 };
