045f6588051635366347d891d2226054f26e9ecc
TIDIE <yuldijasman@gmail.com>
2023-08-21 11:35:17 +0700

    User Login

--- middleware/index.js
+++ middleware/index.js
@@ -3,4 +3,5 @@ var auth = require("./auth");
 var router = express.Router();
 //daftarkan menu registrasi
 router.post("/api/v1/register", auth.registrasi);
+router.post("/api/v1/login", auth.login);
 module.exports = router;
